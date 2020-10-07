e3af9777eac5aa54130c2245d5d3ec7fe4e0ec53
Hugo DEMENEZ <hugo.demenez@isen.yncrea.fr>
2020-10-07 17:01:29 +0200

    Exo5

--- TP4/Exo5/main.c
+++ TP4/Exo5/main.c
@@ -1,7 +1,29 @@
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
+#include <string.h>
 
 int main() {
+	char phrase[1024] ;
+	
+	int nbmot = 1;
+	float caracteres = 0.00;
 
+	printf("Entrez une phrase : ");
+	gets_s(phrase, 1024);
+
+	for (int i = 1; i < 1024; i++) {
+		if (phrase[i] == ' ') {
+			nbmot++;
+		}
+		else {
+			if (phrase[i] != -2) {
+				caracteres++;
+			}
+	
+		}
+	}
+
+	printf("\nIl y a %d mots dans votre phrase", nbmot);
+	printf("\nIl y a en moyenne %.4f caracteres dans vos mots ", caracteres / nbmot);
 }
