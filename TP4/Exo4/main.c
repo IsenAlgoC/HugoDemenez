5324109d9d562015f6c71ec044ca876eca35799f
Hugo DEMENEZ <hugo.demenez@isen.yncrea.fr>
2020-10-07 16:00:29 +0200

    TP4

--- TP4/Exo4/main.c
+++ TP4/Exo4/main.c
@@ -1,16 +1,27 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include "Header.h"
+#include <string.h>
+
+
 int main() {
 	char mot[20];
-
+	char mot2[20];
 
 	printf("\nEntrez votre mot : ");
 	scanf_s("%s", &mot, _countof(mot));
 
 
-	int i; char car; char mot2[20];
-
-
+	int min = 0;
+	int max = strlen(mot) - 1;
+    while (max > min)
+    {
+        if (mot[min++] != mot[max--])
+        {
+            printf("%s n'est pas un Palindrome", mot);
+            return;
+        }
+    }
+    printf("%s est palindrome", mot);
 
 }
