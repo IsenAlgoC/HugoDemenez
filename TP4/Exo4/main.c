1d3289048d96e0d3f25523630c9ab0115c131e30
Hugo DEMENEZ <hugo.demenez@isen.yncrea.fr>
2020-10-07 15:46:37 +0200

    TP4

---
+++ TP4/Exo4/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "Header.h"
+int main() {
+	char mot[20];
+
+
+	printf("\nEntrez votre mot : ");
+	scanf_s("%s", &mot, _countof(mot));
+
+
+	int i; char car; char mot2[20];
+
+
+
+}
