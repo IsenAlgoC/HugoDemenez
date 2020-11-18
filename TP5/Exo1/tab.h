c0659379649dee21686d58c1bcaba1af699f9030
Hugo DEMENEZ <hugo.demenez@isen.yncrea.fr>
2020-11-18 16:19:51 +0100

    TP5

--- TP5/Exo1/tab.h
+++ TP5/Exo1/tab.h
@@ -1,11 +1,14 @@
 #pragma once
 
+//Inclusion des bibliothèques
 #include <stdio.h>
 #include <stdlib.h>
 
+//Déclaration des fonctions
 int initTab(int* tab, int size);
 int afficheTab(int* tab, int size, int nbElts);
 int ajoutElementDansTableau(int* tab, int *size, int *nbElts, int element);
 
+//Déclaration des constantes
 #define TAB2SIZE 100
 #define TAILLEAJOUT 50
